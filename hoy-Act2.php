57a795b838dea1158db531ff0110d1cb0244d69a
perla <perla20468@cbtis75.edu.mx>
2022-10-05 08:10:42 -0500

    Actividades-ps

---
+++ hoy-Act2.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<center>";
+
+echo "Obtener fecha actual con función date() en PHP <br>";
+
+$fechaActual = date('d/m/y h:i:s');
+echo"<br>Fecha con Formato (Dia - Mes - Año) y hora actual <br>"; 
+echo $fechaActual;
+
+echo "</center>";
+
+?>
